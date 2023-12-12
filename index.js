1959d42767b7b6574af115dc7094e01aeda6184e
Mr-React <ambureomkar@gmail.com>
2023-12-12 11:16:03 +0530

    Keyboard interactions and ticker loop

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let app;
 let player;
+let keys = {};
+let keysDiv;
 
 window.onload = function () {
   // Application setup
@@ -18,9 +20,17 @@ window.onload = function () {
   player.y = app.view.height / 2;
   app.stage.addChild(player);
 
-  //mouse interactions
+  // mouse interactions
   app.stage.interactive = true;
   app.stage.on("pointermove", movePlayer);
+
+  // Keyboard interactions
+  window.addEventListener("keydown", keysDown);
+  window.addEventListener("keyup", keysUp);
+
+  // ticker
+  app.ticker.add(gameLoop);
+  keysDiv = document.querySelector("#keys");
 };
 
 function movePlayer(e) {
@@ -29,4 +39,40 @@ function movePlayer(e) {
 
   player.x = pos.x;
   player.y = pos.y;
+}
+
+function keysDown(e) {
+  keys[e.keyCode] = true;
+}
+
+function keysUp(e) {
+  keys[e.keyCode] = false;
+}
+
+function gameLoop() {
+  keysDiv.innerHTML = JSON.stringify(keys);
+
+  // W & up arrow
+  if (keys["87"] || keys["38"]) {
+    console.log("W");
+    player.y = player.y - 5;
+  }
+
+  // S & down arrow
+  if (keys["83"] || keys["40"]) {
+    console.log("S");
+    player.y = player.y + 5;
+  }
+
+  // A & left arrow
+  if (keys["65"] || keys["37"]) {
+    console.log("A");
+    player.x = player.x - 5;
+  }
+
+  // D & right arrow
+  if (keys["68"] || keys["39"]) {
+    console.log("D");
+    player.x = player.x + 5;
+  }
 }
