18cc3b3f04606fc539b0f3d92206c6f858cc6ace
Mr-React <ambureomkar@gmail.com>
2023-12-12 17:21:12 +0530

    Basic with bullets

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let app;
 let player;
-let keys = {};
-let keysDiv;
+let bullets = [];
+let bulletSpeed = 10;
 
 window.onload = function () {
   // Application setup
@@ -11,68 +11,58 @@ window.onload = function () {
     background: 0xaaaaaa,
   });
 
-  document.body.appendChild(app.view);
+  document.querySelector("#gameDiv").appendChild(app.view);
+  app.stage.interactive = true;
+  document
+    .querySelector("#gameDiv")
+    .addEventListener("pointerdown", fireBullet);
 
   // Player Object
   player = PIXI.Sprite.from("/images/player.png");
   player.anchor.set(0.5);
   player.x = app.view.width / 2;
   player.y = app.view.height / 2;
+
   app.stage.addChild(player);
 
-  // mouse interactions
-  app.stage.interactive = true;
-  app.stage.on("pointermove", movePlayer);
-
-  // Keyboard interactions
-  window.addEventListener("keydown", keysDown);
-  window.addEventListener("keyup", keysUp);
-
-  // ticker
   app.ticker.add(gameLoop);
-  keysDiv = document.querySelector("#keys");
 };
 
-function movePlayer(e) {
-  let pos = e.data.global;
-  console.log(pos);
+function fireBullet(e) {
+  console.log("FIRE!!");
 
-  player.x = pos.x;
-  player.y = pos.y;
+  let bullet = createBullet();
+  bullets.push(bullet);
 }
 
-function keysDown(e) {
-  keys[e.keyCode] = true;
+function updateBullets(delta) {
+  for (let i = 0; i < bullets.length; i++) {
+    bullets[i].position.y -= bullets[i].speed;
+
+    if (bullets[i].position.y < 0) {
+      bullets[i].dead = true;
+    }
+  }
+
+  for (let i = 0; i < bullets.length; i++) {
+    if (bullets[i].dead) {
+      app.stage.removeChild(bullets[i]);
+      bullets.splice(i, 1);
+    }
+  }
 }
 
-function keysUp(e) {
-  keys[e.keyCode] = false;
+function gameLoop(delta) {
+  updateBullets(delta);
 }
 
-function gameLoop() {
-  keysDiv.innerHTML = JSON.stringify(keys);
+function createBullet() {
+  let bullet = PIXI.Sprite.from("/images/bullet.png");
+  bullet.anchor.set(0.5);
+  bullet.x = player.x;
+  bullet.y = player.y;
+  bullet.speed = bulletSpeed;
+  app.stage.addChild(bullet);
 
-  // W & up arrow
-  if (keys["87"] || keys["38"]) {
-    console.log("W");
-    player.y = player.y - 5;
-  }
-
-  // S & down arrow
-  if (keys["83"] || keys["40"]) {
-    console.log("S");
-    player.y = player.y + 5;
-  }
-
-  // A & left arrow
-  if (keys["65"] || keys["37"]) {
-    console.log("A");
-    player.x = player.x - 5;
-  }
-
-  // D & right arrow
-  if (keys["68"] || keys["39"]) {
-    console.log("D");
-    player.x = player.x + 5;
-  }
+  return bullet;
 }
