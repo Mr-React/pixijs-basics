4070256ba04bb4ad7c3b2b4d11119ec8a0dd256c
Mr-React <ambureomkar@gmail.com>
2023-12-12 10:38:26 +0530

    mouse interaction

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let app;
+let player;
 
 window.onload = function () {
+  // Application setup
   app = new PIXI.Application({
     width: 800,
     height: 600,
@@ -8,4 +10,23 @@ window.onload = function () {
   });
 
   document.body.appendChild(app.view);
+
+  // Player Object
+  player = PIXI.Sprite.from("/images/player.png");
+  player.anchor.set(0.5);
+  player.x = app.view.width / 2;
+  player.y = app.view.height / 2;
+  app.stage.addChild(player);
+
+  //mouse interactions
+  app.stage.interactive = true;
+  app.stage.on("pointermove", movePlayer);
 };
+
+function movePlayer(e) {
+  let pos = e.data.global;
+  console.log(pos);
+
+  player.x = pos.x;
+  player.y = pos.y;
+}
